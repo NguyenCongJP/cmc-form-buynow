1a5557cc6ce69a6128de8bd477c36733b62d0251
NguyenCongJP <nguyenquecong.nqc@gmail.com>
2021-11-30 16:47:12 +0700

    add cmc value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './component/form/form.js';
-import FormTest from './component/form/formTest.js';
 import './assest/css/style.css'
 
 function App() {
@@ -9,7 +7,6 @@ function App() {
         <div className="App">
             <div className="main-content">
                 <Form />
-                {/* <FormTest /> */}
             </div>
         </div>
     );
